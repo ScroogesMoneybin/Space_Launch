bd140d54bf2b17be1044cc67a3819f9241b26235
Nick <kammer@gmail.com>
2023-06-01 00:06:02 -0400

    fifth commit

--- client/src/hooks/requests.js
+++ client/src/hooks/requests.js
@@ -1,4 +1,4 @@
-const API_URL = 'http://localhost:8000/v1' || process.env.HOST_URL;
+const API_URL = process.env.HOST_URL;
 
 async function httpGetPlanets() {
   // Load planets and return as JSON.
