668dc061ffde477a0279e310258118b51c178b78
Nick <kammer@gmail.com>
2023-06-01 00:56:49 -0400

    adjust request URL commit

--- client/src/hooks/requests.js
+++ client/src/hooks/requests.js
@@ -1,5 +1,6 @@
-const API_URL_BASE = 'https://space-launch.onrender.com/';
+const API_URL_BASE = 'https://space-launch.onrender.com';
 const API_URL = `${API_URL_BASE}/v1`;
+console.log(API_URL)
 
 async function httpGetPlanets() {
   // Load planets and return as JSON.
